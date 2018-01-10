d5e817c5f86284c91a55c8dddc0b49cdc2d95f37
Don Oerkfitz <don@oerkfitz.com>
2018-01-10 15:20:40 -0600

    added franz recipe files

---
+++ webview.js
@@ -0,0 +1,17 @@
+// gpm integration
+/*jshint esversion: 6 */
+module.exports = (Franz) => {
+    function getMessages() {
+        let direct = 0;
+        let indirect = 0;
+        const FranzData = document.querySelector("#FranzMessages").dataset;
+        if (FranzData) {
+            direct = FranzData.direct;
+            indirect = FranzData.indirect;
+        }
+
+        Franz.setBadge(direct, indirect);
+    }
+
+    Franz.loop(getMessages);
+};
