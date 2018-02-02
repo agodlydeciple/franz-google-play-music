0297c6e0a2bde0ab43ba6ef2d1cd3fa5fb869e7b
Don Oerkfitz <don@oerkfitz.com>
2018-02-02 15:59:50 -0600

    working on new features.

--- webview.js
+++ webview.js
@@ -6,20 +6,48 @@ module.exports = (Franz) => {
     function Main() {
         try {
             let nowPlaying = getNowPlaying();
+            let playingStatus;
+            let styles = path.join(__dirname, "css/custom-style.css");
+            let hardStyle = 
 
-            // let styles = path.join(__dirname, "css/custom-style.css");
+            Franz.injectCSS(styles);
 
-            // Franz.injectCSS(styles);
+            //> playingStatus = 1 --> music is playing
+            //> playingStatus = 2 --> music is paused
+            //> playingStatus = 0 --> music is stopped
+            if (document.getElementById("player-bar-play-pause") !== null) {
+                if (document.getElementById("player-bar-play-pause").getAttribute("title") === "Pause") {
+                    playingStatus = 1;
+                } else {
+                    playingStatus = 2;
+                }
+            } else {
+                playingStatus = 0;
+            }
 
-            Franz.setBadge(1);
+            //> used for development, only broadcast the nowPlaying variable once.
+            if (nowPlaying !== wasPlaying) {
+                console.log(nowPlaying);
+                wasPlaying = nowPlaying;
+            }
+
+            Franz.setBadge(playingStatus);
         } catch (error) {
             console.log(error);
         }
     }
 
     function getNowPlaying() {
-        return (document.getElementById("currently-playing-title").firstChild.data + " - " + document.getElementById("player-artist").firstChild.data);
+        let rv;
+        if ((document.getElementById("currently-playing-title") !== null) && (document.getElementById("player-artist") !== null)) {
+            rv = document.getElementById("currently-playing-title").firstChild.data + " - " + document.getElementById("player-artist").firstChild.data;
+        } else {
+            rv = "Nothing Playing";
+        }
+
+        return rv;
     }
 
+    let wasPlaying;
     Franz.loop(Main);
 };
