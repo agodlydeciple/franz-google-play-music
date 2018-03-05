1ee89fee4c8f336a0c05f59e9d796c74bfd5137f
Don Oerkfitz <don@oerkfitz.com>
2018-03-05 13:35:20 -0600

    commented out code that caused interface to lock up

--- webview.js
+++ webview.js
@@ -6,30 +6,30 @@ module.exports = (Franz) => {
     function Main() {
         try {
             let nowPlaying = getNowPlaying();
-            let playingStatus;
-            let styles = path.join(__dirname, "css/custom-style.css");
-            let hardStyle = 
+            let playingStatus = 0;
+            // let styles = path.join(__dirname, "css/custom-style.css");
+            // let hardStyle;
 
-            Franz.injectCSS(styles);
+            // Franz.injectCSS(styles);
 
             //> playingStatus = 1 --> music is playing
             //> playingStatus = 2 --> music is paused
             //> playingStatus = 0 --> music is stopped
-            if (document.getElementById("player-bar-play-pause") !== null) {
-                if (document.getElementById("player-bar-play-pause").getAttribute("title") === "Pause") {
-                    playingStatus = 1;
-                } else {
-                    playingStatus = 2;
-                }
-            } else {
-                playingStatus = 0;
-            }
+            // if (document.getElementById("player-bar-play-pause") !== null) {
+            //     if (document.getElementById("player-bar-play-pause").getAttribute("title") === "Pause") {
+            //         playingStatus = 1;
+            //     } else {
+            //         playingStatus = 2;
+            //     }
+            // } else {
+            //     playingStatus = 0;
+            // }
 
             //> used for development, only broadcast the nowPlaying variable once.
-            if (nowPlaying !== wasPlaying) {
-                console.log(nowPlaying);
-                wasPlaying = nowPlaying;
-            }
+            // if (nowPlaying !== wasPlaying) {
+            //     console.log(nowPlaying);
+            //     wasPlaying = nowPlaying;
+            // }
 
             Franz.setBadge(playingStatus);
         } catch (error) {
@@ -48,6 +48,6 @@ module.exports = (Franz) => {
         return rv;
     }
 
-    let wasPlaying;
+    // let wasPlaying;
     Franz.loop(Main);
 };
